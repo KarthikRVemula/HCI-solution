9cb001ec067ccfe4e0f695c06ee20021301f85dc
KarthikRVemula <cqdn1000@leeds.ac.uk>
2025-12-06 19:27:17 +0000

    Fixed errors 1 & 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -224,7 +224,7 @@ private const val ERROR_404_HTML =
                 text-decoration: underline;
             }
             a:focus {
-                outline: 3px solid #4A90E2;
+                outline: 3px solid #000;
                 outline-offset: 2px;
             }
         </style>
